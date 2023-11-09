9eae349f208b29e1313d7a5e40e08efbabab8681
just-renis <rrenis5642@gmail.com>
2023-11-09 17:26:29 +0200

    more api sql requests.

--- api/app.js
+++ api/app.js
@@ -10,36 +10,30 @@ const db = mysql.createConnection({
   database: "maisto_produktu_sistema",
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL');
-  }
-});
+db.connect((err) => { err ? console.error('Error connecting to MySQL:', err) : console.log('Connected to MySQL'); });
 
-app.get('/api/categories', (req, res) => {
+app.get('/api/resources/categories/products', (req, res) => {
   const query = 
   `SELECT
     c.id,
     c.name,
     COUNT(p.id) as product_count,
     JSON_ARRAYAGG(
-      JSON_OBJECT("name", p.name, "price", p.price, "description", p.description)
+      JSON_OBJECT("name", p.name, "price", p.price, "description", p.description, "quantity", p.quantity, "weight", p.weight, 
+      "type", p.type, "producer", p.producer, "seller", p.seller, "discount", p.discount)
     ) as products
   FROM categories c
   LEFT JOIN products p ON c.id = p.category_id
   GROUP BY c.id, c.name`;
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Error fetching data from MySQL:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(results);
-    }
-  });
+  db.query(query, (err, results) => { err ? res.status(500).json({ error: 'Internal Server Error' }) : res.json(results); });
 });
 
-app.listen(port, () => {
-  console.log(`API server is running on port ${port}`);
+app.get('/api/resources/products/types', (req, res) => {
+  db.query('SELECT DISTINCT type FROM products', (err, results) => { err ? res.status(500).json({ error: 'Internal Server Error' }) : res.json(results); });
 });
+
+app.get('/api/resources/categories', (req, res) => {
+  db.query('SELECT name FROM categories', (err, results) => { err ? res.status(500).json({ error: 'Internal Server Error' }) : res.json(results); });
+});
+
+app.listen(port, () => { console.log(`API server is running on port ${port}`); });
